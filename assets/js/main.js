d2759428b6aafdc90018beae4becffae81ef4f90
NetoPetit <cidao_petit5@hotmail.com>
2023-09-05 00:51:55 -0300

    atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-console.log('Sucesso!')
+
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then(function (response) {
+        console.log(response)
+    })
+    .catch(function (error) {
+        console.error(error)    
+    })
+    .finally(function (){
+        console.log('Requisição concluída!')
+    })
+
+
+const x = 10 + 10
+console.log(x)
